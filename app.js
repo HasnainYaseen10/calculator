575d43a500b272a9f42d6b2fd67ad07aa5ac5e9c
hasnain <hasnain1.yaseen@gmail.com>
2024-03-14 10:00:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let cal = prompt('enter your number *' , '');
+let cal1=prompt('enter your number *' , '');
+let result=cal*cal1
+alert(result);
+
+
+let cal2 = prompt('enter your number -' , '');
+let cal3=prompt('enter your number -' , '');
+let result2=cal2-cal3
+alert(result2);
+
+
+let cal4 =Number (prompt('enter your number +' , ''));
+let cal5=Number (prompt('enter your number +' , ''));
+let result3=cal4+cal5
+alert(result3);
+
+
+let cal6 = prompt('enter your number /' , '');
+let cal7=prompt('enter your number /' , '');
+let result4=cal6/cal7
+alert(result4);
+
+
